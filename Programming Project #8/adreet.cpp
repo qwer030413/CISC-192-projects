2afe7645943749b0fd45ba10ec323087dea44c01
Chris <comosjay@gmail.com>
2023-10-26 23:53:25 -0700

    adreeeeeet

---
+++ Programming Project #8/adreet.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <math.h>       
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void simplify(int num, int den, int which)
+{
+   //for loop
+   //if thing
+
+    //if which == 1
+    //str.numerator1 = num
+    //str.denominator1 = den
+    
+
+    //else if(which == 2)
+    //str.numerator2 = num
+    //str.denominator2 = den
+}
+int main(){
+
+}
