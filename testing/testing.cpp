d46b1fb92655a39bbfef1488975d5b7a115c8709
Chris <comosjay@gmail.com>
2023-12-08 03:23:04 -0800

    a

--- testing/testing.cpp
+++ testing/testing.cpp
@@ -12,25 +12,7 @@
 using namespace std;
 
 int main(){
-<<<<<<< HEAD
-    double a = 40;
-    double& b = a;
-    b = 10;
-    cout << a;
-=======
-    int zipcode = 1;
-    int meow;
-    switch(1)
-    {
-        case 1:
-            meow = 1;
-        case 2:
-            meow = 1 + 2;
-
-        default:
-            meow = 4;
-    }
-
-    cout << meow;
->>>>>>> 8d05f16d1277bd1ac52625f07017321d8443fcb5
+    char a[20] = "";
+    // char b[20] = {'/0'};
+    char c[20] = {0};
 }
